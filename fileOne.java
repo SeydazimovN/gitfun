0dc42152bbf4db45279cc9b9606c08118782146a
seydazimovN <theycallmeseyda@gmail.com>
2019-01-08 13:19:44 +0600

    refactor

--- fileOne.java
+++ fileOne.java
@@ -1,3 +1,12 @@
 public void initialCode() {
 	// Initial code
 }
+
+public void refactorOne() {
+	// REFACTOR 1
+}
+
+public void refactorTwo() {
+	// REFACTOR 2
+}
+
--- fileTwo.java
+++ fileTwo.java
@@ -1,3 +1,23 @@
 public void initialCode() {
 	// Initial code
 }
+
+public void refactorOne() {
+	// REFACTOR 1
+}
+
+public void refactorTwo() {
+	// REFACTOR 2
+}
+
+public void refactorThree() {
+	// REFACTOR 3
+}
+
+public void refactorFour() {
+	// REFACTOR 4
+}
+
+public void refactorFive() {
+	// REFACTOR 5
+}
