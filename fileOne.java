87c580bf391b2ef4690d50414f6b1e5a8643f746
seydazimovN <theycallmeseyda@gmail.com>
2019-01-08 13:20:35 +0600

    test

--- fileOne.java
+++ fileOne.java
@@ -2,11 +2,27 @@ public void initialCode() {
 	// Initial code
 }
 
+public void testOne() {
+	// TEST 1
+}
+
+public void testTwo() {
+	// TEST 2
+}
+
 public void refactorOne() {
 	// REFACTOR 1
 }
 
+public void testThree() {
+	// TEST 3
+}
+
 public void refactorTwo() {
 	// REFACTOR 2
+}
+
+public void testFour() {
+	// TEST 4
 }
 
--- fileTwo.java
+++ fileTwo.java
@@ -6,8 +6,20 @@ public void refactorOne() {
 	// REFACTOR 1
 }
 
+public void testOne() {
+	// TEST 1
+}
+
 public void refactorTwo() {
 	// REFACTOR 2
+}
+
+public void testTwo() {
+	// TEST 2
+}
+
+public void testThree() {
+	// TEST 3
 }
 
 public void refactorThree() {
